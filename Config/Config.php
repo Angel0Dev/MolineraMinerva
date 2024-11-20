94732a78fe6589b99b8dc2b8cf90fd2bf0a3753c
XAngeloX-Code <angelofloresmolero25@hotmail.com>
2024-11-20 18:48:31 -0500

    Vercel

--- Config/Config.php
+++ Config/Config.php
@@ -7,10 +7,10 @@ const DB = "gestion_archivos";
 const CHARSET = "charset=utf8";
 // configuracion para resetar contraseña
 // si no configuras no funcionará el envio de correo
-const HOST_SMTP = "smtp.gmail.com";
-const USER_SMTP = 'angelofloresmolero25@gmail.com';
-const CLAVE_SMTP = 'kzkbwwasbkaydanw';
+const HOST_SMTP = "smtp.gmail.com";   //servidor de correo
+const USER_SMTP = 'angelofloresmolero25@gmail.com';  //correo que conectara al servidor de gmail
+const CLAVE_SMTP = 'kzkbwwasbkaydanw'; //clave del correo que conectara al servidor de gmail
 const PUERTO_SMTP = 465;
-const CORREO_FROM = 'angelo2020molero@gmail.com';
-const TITLE = 'Prueba';
+const CORREO_FROM = 'angelo2020molero@gmail.com';  //correo que se mostrara en el remitente
+const TITLE = 'Prueba'; //titulo del correo
 ?>
